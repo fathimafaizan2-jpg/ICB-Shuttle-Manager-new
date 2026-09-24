f1d2eab6a54ba563b64e36f74aa05c3e4875a6eb
fathimafaizan2-jpg <fathimafaizan2@gmail.com>
2026-09-24 13:01:32 +0300

    Allow serving dotfiles in static files

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ dotenv.config();
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const app = express();
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public"), { dotfiles: "allow" }));
 
 const JWT_SECRET = process.env.JWT_SECRET;
 const LOCK_AFTER_ATTEMPTS = 5;
